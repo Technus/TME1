0975a18e434a9303b2d9fdd7734eda338a7cd0ae
Tec <daniel112092@gmail.com>
2024-07-08 16:26:40 +0200

    Draft API

---
+++ API/Controllers/RobotController.cs
@@ -0,0 +1,31 @@
+﻿using LanguageExt;
+using Microsoft.AspNetCore.Mvc;
+using TME1.Abstractions.Repositories;
+using TME1.Core.DataTransferObjects;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("robot/[controller]")]
+public class RobotController : RobotControllerBase<int, RobotDTO>
+{
+  public RobotController(ILogger<RobotController> logger, IRobotRepository<int, RobotDTO> repository) : base(logger, repository)
+  {
+  }
+
+  protected override void LogGetAllAsyncError(Fin<RobotDTO> result) => Logger.LogGetAllAsyncError(result);
+  protected override void LogStateUpdateError(Fin<RobotDTO> result) => Logger.LogStateUpdateError(result);
+}
+
+public static partial class RobotControllerLogExtensions
+{
+  [LoggerMessage(
+    Level = LogLevel.Error,
+    Message = "Could not load robot/s: `{result}`")]
+  public static partial void LogGetAllAsyncError(this ILogger logger, Fin<RobotDTO> result);
+
+  [LoggerMessage(
+    Level = LogLevel.Error,
+    Message = "Could not update state: `{result}`")]
+  public static partial void LogStateUpdateError(this ILogger logger, Fin<RobotDTO> result);
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using TME1.Abstractions.Repositories;
+using TME1.Abstractions.Services;
+using TME1.Core;
+using TME1.Core.DataTransferObjects;
+using TME1.Core.Repositories;
+using TME1.Core.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,13 @@ builder.Services.AddControllers()
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<RobotContext>(config 
+  => config.UseSqlServer(builder.Configuration["TME1"], options 
+    => options.MigrationsAssembly(typeof(RobotContext).Assembly.GetName().Name)), ServiceLifetime.Singleton);
+
+builder.Services
+  .AddSingleton<IRobotRepository<int, RobotDTO>, RobotRepository>()
+  .AddSingleton<IRobotStateService<int>, RobotStateService>();
 
 var app = builder.Build();
 
