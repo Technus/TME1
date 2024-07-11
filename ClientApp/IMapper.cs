648b9f00bbf8b0baf35798eb99a9479af5dd93e3
Tec <daniel112092@gmail.com>
2024-07-11 18:11:10 +0200

    Add mapper implementation

--- ClientApp/IMapper.cs
+++ ClientApp/IMapper.cs
@@ -1,4 +1,24 @@
-﻿namespace TME1.ClientApp;
+﻿using TME1.ClientApp.ViewModels;
+using TME1.ClientCore.Models;
+
+namespace TME1.ClientApp;
+
+/// <summary>
+/// Simple interface to abstract mapping done in the application
+/// </summary>
 public interface IMapper
 {
+  /// <summary>
+  /// Converts robot view model to it's model
+  /// </summary>
+  /// <param name="viewModel"></param>
+  /// <returns></returns>
+  RobotModel MapToViewModel(RobotTileViewModel viewModel);
+
+  /// <summary>
+  /// Converts robot model to it's view model
+  /// </summary>
+  /// <param name="model"></param>
+  /// <returns></returns>
+  RobotTileViewModel MapToModel(RobotModel model);
 }
--- ClientApp/Mapper.cs
+++ ClientApp/Mapper.cs
@@ -1,11 +1,16 @@
 ﻿using Riok.Mapperly.Abstractions;
+using TME1.ClientApp.ViewModels;
+using TME1.ClientCore.Models;
 
 namespace TME1.ClientApp;
 /// <summary>
-/// Mapper Initialization
+/// Mapper implementation using source generator
 /// </summary>
 [Mapper]
 public partial class Mapper : IMapper
 {
-
+  ///<inheritdoc/>
+  public partial RobotTileViewModel MapToModel(RobotModel model);
+  ///<inheritdoc/>
+  public partial RobotModel MapToViewModel(RobotTileViewModel viewModel);
 }
